531645c37eee082f624bb4a11a66b201b869487d
Akib Balkhi <akibbalkhiad@gmail.com>
2018-10-30 23:49:12 +0530

    Updated print statement

--- google_search_using_speech.py
+++ google_search_using_speech.py
@@ -22,6 +22,6 @@ try:
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    print("Failed to request results from Google Speech Recognition service; {0}".format(e))
 
 
